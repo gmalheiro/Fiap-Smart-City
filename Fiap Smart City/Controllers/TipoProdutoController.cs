900c002f35ee48f10e2c127260e5f0987194dbce
Gabriel Malheiro <g.silva.malheiro@avanade.com>
2022-11-28 14:39:12 -0300

    Added a controller and a view

---
+++ Fiap Smart City/Controllers/TipoProdutoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Fiap_Smart_City.Controllers
+{
+    public class TipoProdutoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
